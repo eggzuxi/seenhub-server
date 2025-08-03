5a56c2ee201757611a142c99d1b503d51dd3c160
eggzuxi <eggzuxi99@gmail.com>
2025-08-03 15:18:00 +0900

    :tada: init: 프로젝트 환경설정 및 데이터베이스 연결

---
+++ src/test/java/com/seenhub/backend/SeenhubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seenhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeenhubBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
