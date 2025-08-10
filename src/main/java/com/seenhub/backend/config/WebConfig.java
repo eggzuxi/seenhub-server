3facb267035d9161907b92dbf7f1ee61898223a3
eggzuxi <eggzuxi99@gmail.com>
2025-08-10 17:17:08 +0900

    :sparkles: feat: CORS 설정

---
+++ src/main/java/com/seenhub/backend/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.seenhub.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import static com.seenhub.backend.constants.ApiConstants.CLIENT_BASE_URL;
+import static com.seenhub.backend.constants.ApiConstants.CLIENT_DEV_URL;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(CLIENT_BASE_URL, CLIENT_DEV_URL)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
---
+++ src/main/java/com/seenhub/backend/constants/ApiConstants.java
@@ -0,0 +1,18 @@
+package com.seenhub.backend.constants;
+
+public class ApiConstants {
+
+    public static final String USER_BASE_URL = "/api/user";
+    public static final String MUSIC_BASE_URL = "/api/music";
+    public static final String MUSIC_LIST_URL = "/api/music/all";
+    public static final String MOVIE_BASE_URL = "/api/movie";
+    public static final String MOVIE_LIST_URL = "/api/movie/all";
+    public static final String SERIES_BASE_URL = "/api/series";
+    public static final String SERIES_LIST_URL = "/api/series/all";
+    public static final String BOOK_BASE_URL = "/api/book";
+    public static final String BOOK_LIST_URL = "/api/book/all";
+
+    public static final String CLIENT_BASE_URL = "https://seenhub.site";
+    public static final String CLIENT_DEV_URL = "http://localhost:8080";
+
+}
